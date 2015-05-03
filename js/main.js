420dada1acac3374989c0a30024c0ec911f09754
Morten Noddeland <morten@noddeland.no>
2015-05-03 16:28:30 +0200

    Completed os detection, added some guides and tweaked the code

--- js/main.js
+++ js/main.js
@@ -1,55 +1,57 @@
+// When the 'change OS' link is clicked, show the OS select box
 $('.change-os-button').click(function(event) {
   event.preventDefault();
   $(this).hide();
   $(".change-os-select").show();
 });
 
+// When an OS is selected from the select box, change what guide is being shown
 $('.change-os-select').change(function() {
-  var os = $('.change-os-select option:selected').val();
+  var $selected_os = $('.change-os-select option:selected');
+  $('.os-name').text($selected_os.text());
   $('.instructions').hide();
-  $('.instructions.' + os).show();
+  $('.instructions.' + $selected_os.val()).show();
 });
 
-var detected_os = platform.os;
+// -------------------------------------------------
+// OS DETECTION
+// -------------------------------------------------
+// The instruction_os variable determines which guide should be shown to the user.
+// For example, if platform.js detects that you are running Windows 7, "instruction_os"
+// will be set to "win-7". To find what the value of instruction_os should be,
+// check the values in the select box .change-os-select
 var instruction_os;
 
-console.log(detected_os);
-
-if (detected_os.family === 'Windows') {
-  var os_version = detected_os.version;
-  if (os_version.match(/10.7/g)) { // Lion
-    instruction_os = 'osx-10.7';
-  }
-  else if (os_version.match(/10.8/g)) { // Mountain Lion
-    instruction_os = 'osx-10.8';
-  }
-  else {
-    instruction_os = 'osx-10.10'; // Show newest
-  }
+if (platform.os.family === 'Windows') {
+  var windows_version = platform.os.version.substring(0, 1); // Only get the first number
+  if      (windows_version.match(/6/g))         { instruction_os = 'win-vista'; }
+  else if (windows_version.match(/7/g))         { instruction_os = 'win-7'; }
+  else if (windows_version.match(/8/g))         { instruction_os = 'win-8'; }
+  else                                          { instruction_os = 'win-8'; }  // Newest Windows guide
 }
-
-else if (detected_os.family === 'OS X') {
-  var os_version = detected_os.version;
-  if (os_version.match(/10.7/g)) { // Lion
-    instruction_os = 'osx-10.7';
-  }
-  else if (os_version.match(/10.8/g)) { // Mountain Lion
-    instruction_os = 'osx-10.8';
-  }
-  else if (os_version.match(/10.9/g)) { // Mavericks
-    instruction_os = 'osx-mavericks';
-  }
-  else if (os_version.match(/10.10/g)) { // Yosemite
-    instruction_os = 'osx-yosemite';
-    console.log(instruction_os);
-  }
-  else {
-    instruction_os = 'osx-10.10'; // Show newest
-  }
+else if (platform.os.family === 'OS X') {
+  if      (platform.os.version.match(/10.7/g))  { instruction_os = 'osx-lion'; }
+  else if (platform.os.version.match(/10.8/g))  { instruction_os = 'osx-mountain-lion'; }
+  else if (platform.os.version.match(/10.9/g))  { instruction_os = 'osx-mavericks'; }
+  else if (platform.os.version.match(/10.10/g)) { instruction_os = 'osx-yosemite'; }
+  else                                          { instruction_os = 'osx-yosemite'; } // Newest OS X guide
 }
+else if (platform.os.family === 'iOS')          { instruction_os = 'ios'; }
+else if (platform.os.family === 'Android')      { instruction_os = 'android'; }
+else if (platform.os.family === 'CentOS' ||
+         platform.os.family === 'Debian' ||
+         platform.os.family === 'Fedora' ||
+         platform.os.family === 'FreeBSD' ||
+         platform.os.family === 'Kubuntu' ||
+         platform.os.family === 'Linux Mint' ||
+         platform.os.family === 'Red Hat' ||
+         platform.os.family === 'SuSE' ||
+         platform.os.family === 'Ubuntu' ||
+         platform.os.family === 'Xubuntu' ||
+         platform.os.family === 'Linux')        { instruction_os = 'linux'; }
+else                                            { instruction_os = 'general'; }
 
-
+// When the page loads, show the guide for the detected OS
 $('.change-os-select').val(instruction_os);
 $('.instructions.' + instruction_os).show();
-// $('.instructions.' + 'win-7').show();
 $('.os-name').text($('.change-os-select option:selected').text());
